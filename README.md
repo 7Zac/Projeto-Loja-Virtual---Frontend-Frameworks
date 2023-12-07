21b1ccae812c28b7eb1f48222c806537929701e6
matheusmouraa10 <matheusmourasilvaa10@gmail.com>
2023-12-07 13:53:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vueshop Isaac Lopes e Marcos Antonio
+Utilizem para criação base da loja virtual.
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HeaderMain></HeaderMain>
+  <ProductsMain></ProductsMain>
+  <FooterMain></FooterMain>
+  <main>
+    <router-view/>
+  </main>
+</template>
+
+<script>
+  import HeaderMain from './components/HeaderMain.vue';
+  import ProductsMain from './components/ProductsMain.vue';
+  import FooterMain from './components/FooterMain.vue';
+  
+  export default{
+    components: {
+      HeaderMain,
+      ProductsMain,
+      FooterMain
+    }
+  }
+</script>
+
+<style>
+</style>
